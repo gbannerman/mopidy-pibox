1c72a0155ea6cc4eb58b59232356644705889e9c
gavinbannerman <bannermangavin@gmail.com>
2017-07-06 23:14:40 +0100

    attempted playback

--- mopidy_pibox/pibox_web/web.py
+++ mopidy_pibox/pibox_web/web.py
@@ -4,7 +4,11 @@ import os
 
 import tornado.web
 
-from mopidy import ext
+from mopidy import audio, backend as backend_api, models, ext
+
+import spotify
+
+from mopidy_spotify import backend, playback
 
 class MyRequestHandler(tornado.web.RequestHandler):
     def initialize(self, core):
@@ -14,3 +18,13 @@ class MyRequestHandler(tornado.web.RequestHandler):
         self.write(
             'Hello, world! This is Mopidy %s' %
             self.core.get_version().get())
+        play_song()
+
+def play_song():
+    spot_backend = backend.SpotifyBackend()
+    
+    playback_prov = spot_backend.playback
+    
+    uri = 'spotify:track:0Lx6O1tC3CPF1giLJIt5Jv'
+    track = models.Track(uri=uri)
+    playback_prov.change_track(track)
