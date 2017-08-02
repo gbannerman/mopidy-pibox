e55139380c99fade20eee380eaae5746e73d26c4
gavinbannerman <bannermangavin@gmail.com>
2017-08-02 22:32:28 +0100

    styling invalid page

--- mopidy_pibox/pibox_web/session.py
+++ mopidy_pibox/pibox_web/session.py
@@ -1,7 +1,7 @@
 class PiboxSession(object):
 
 	def __init__(self):
-		self.blacklist = ['spotify:track:312A8WfROSLvZbMDHBUPDp', 'spotify:track:42F4K181FFdhkbDAaoxDye']
+		self.blacklist = ['spotify:track:7nmZWhQDgjroCzKg71OPt7']
 		self.count = 0
 		self.votes = {}
 		
