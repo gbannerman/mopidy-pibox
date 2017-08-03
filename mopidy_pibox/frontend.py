37a9f5b968f394a939f2f01c097963270f0983f3
gavinbannerman <bannermangavin@gmail.com>
2017-08-04 00:12:08 +0100

    passing message to frontend

--- mopidy_pibox/frontend.py
+++ mopidy_pibox/frontend.py
@@ -2,6 +2,7 @@ import pykka
 import random
 
 from mopidy import core
+from pibox_web import web
 
 
 class PiboxFrontend(pykka.ThreadingActor, core.CoreListener):
@@ -16,7 +17,12 @@ class PiboxFrontend(pykka.ThreadingActor, core.CoreListener):
 
 	def track_playback_ended(self, tl_track, time_position):
 		if self.core.tracklist.get_length().get() == 0:
-			new_track_uri = random.choice(self.core.playlists.get_items(self.uri).get()).uri
+			playlist = self.core.playlists.get_items(self.uri).get()
+			new_track_uri = None
+			attempts = 0
+			while ((played_already(uri, self.core) or in_tracklist(uri, self.core)) and attempts < len(playlist)):
+				new_track_uri = random.choice(playlist).uri
+				attempts += 1
 			self.core.tracklist.add(uri=new_track_uri, at_position=0).get()
 			if self.core.playback.get_state().get() == core.PlaybackState.STOPPED:
 				self.core.playback.play()
