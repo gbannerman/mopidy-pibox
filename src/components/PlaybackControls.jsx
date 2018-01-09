a492e380d8eafaec26a8e80dccb41e6eb577d59b
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-01-09 11:14:50 +0000

    fixed playing issue

--- src/components/PlaybackControls.jsx
+++ src/components/PlaybackControls.jsx
@@ -26,7 +26,7 @@ export default class PlaybackControls extends React.Component {
 
 		return (
 
-			<button onClick={this.toggle.bind(this)}>{ playing ? "PAUSE" : "PLAY" }</button>
+			<button onClick={this.toggle.bind(this)}>{ this.state.playing ? "PAUSE" : "PLAY" }</button>
 		);
 	}
 }
