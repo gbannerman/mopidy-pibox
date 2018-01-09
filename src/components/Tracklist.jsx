8d1f1bebef6a70b3c69858b1e83a65ec622900b7
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-01-09 15:30:24 +0000

    testing tracklist limit

--- src/components/Tracklist.jsx
+++ src/components/Tracklist.jsx
@@ -5,7 +5,11 @@ export default class Tracklist extends React.Component {
 
 	render() {
 
-		const mappedTracks = this.props.tracks.slice(1, 1 + this.props.display).map((track, index) => <TracklistItem key={index} track={track}/>);
+		const toDisplay = this.props.tracks.slice(1, 1 + this.props.display);
+		console.log(this.props.display);
+		console.log(toDisplay);
+		const mappedTracks = toDisplay.map((track, index) => <TracklistItem key={index} track={track}/>);
+
 
 		return (
 
