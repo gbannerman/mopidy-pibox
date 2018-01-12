0bee0d1e35bb6c6dd44fde0ea9269caba722691b
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-01-12 16:23:41 +0000

    adding loading spinner while searching

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import SearchBox from './SearchBox.jsx';
 import SearchResultItem from './SearchResultItem.jsx';
 import '../style/Search.css';
+import { getMopidy } from '../App.js';
+
+var Spinner = require('react-spinkit');
 
 export default class Search extends React.Component {
 
@@ -9,31 +12,53 @@ export default class Search extends React.Component {
     super(props);
     this.state = {
       tracks: [],
+      loading: true
     };
   }
 
-  search(results) {
-  	if (results[0]) {
-    	this.setState({tracks: results[0].tracks});
-    } else {
-    	this.setState({tracks: []});
-    }
+  search(e) {
+    this.setState({loading: true});
+    e.preventDefault();
+    let queryParameters = e.target.query.value.split(" ");
+    getMopidy().library.search({'any': queryParameters}, ['spotify:'], false).done((results) => {
+      if (results[0]) {
+        this.setState({tracks: results[0].tracks});
+      } else {
+        this.setState({tracks: []});
+      }
+      this.setState({loading: false});
+    });
   }
 
 	render() {
 
 		const searchResults = this.state.tracks.map((track, index) => <SearchResultItem key={index} track={track} tracklist={this.props.tracklist}/>);
 
+    let results;
+
+    if (this.state.loading) {
+      results = (
+        <div className="loading">
+            <h3>searching...</h3>
+            <Spinner fadeIn="quarter" name="double-bounce" />
+        </div>
+      );
+    } else {
+      results = (
+        <div className="results">
+          <table>
+            <tbody>
+              { searchResults }
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+
 		return (
 			<div>
 				<SearchBox handleSubmit={ this.search.bind(this) } />
-				<div className="results">
-					<table>
-            <tbody>
-						  { searchResults }
-            </tbody>
-					</table>
-				</div>
+        { results }
 			</div>
 		);
 	}
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,17 +4,11 @@ import { getMopidy } from '../App.js';
 
 export default class SearchBox extends React.Component {
 
-	processData(e) {
-		e.preventDefault();
-		let queryParameters = e.target.query.value.split(" ");
-		getMopidy().library.search({'any': queryParameters}, ['spotify:'], false).done(this.props.handleSubmit);
-	}
-
 	render() {
 
 		return (
 
-			<form onSubmit={this.processData.bind(this)} method="POST">
+			<form onSubmit={this.props.handleSubmit} method="POST">
         <input name="query" type="text" id="searchField" placeholder="search songs, artists, etc..." autoComplete="off"/>
         <input type="submit" name="submit" />
       </form>
