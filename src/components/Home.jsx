2b41f4ece612a580f0189fe43d83e9d19d0849d6
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-01-17 09:44:57 +0000

    changed the now playing styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,9 +13,8 @@ export default class Home extends React.Component {
 			<div>
         <ul>
         	<li className="nav-item-title"><h2 className="nav-title">pibox</h2></li>
-        	<li className="nav-item-search"><Link className="Link" to="/pibox/search"><img className="icon" alt="Search icon" src="https://d30y9cdsu7xlg0.cloudfront.net/png/14173-200.png" /></Link></li>
+        	<li className="nav-item-search"><Link className="Link" to="/pibox/search"><img className="icon" alt="search icon" src="https://d30y9cdsu7xlg0.cloudfront.net/png/14173-200.png" /></Link></li>
         </ul>
-	      <h3>Now Playing</h3>
 	      <NowPlaying image={this.props.image} track={this.props.nowPlaying} />
 	      <Tracklist tracks={this.props.tracklist} display={5} />
 				<PlaybackControls playing={this.props.playing} />
