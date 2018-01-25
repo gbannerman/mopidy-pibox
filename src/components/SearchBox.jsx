9c13884ed88d945c5d8c112cd6c38da149fb1a33
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-01-25 11:29:25 +0000

    trying to fix autofocus on search

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form'
 import '../style/SearchBox.css';
 
-let SearchBox = props => {
+class SearchBox extends React.Component {
 
-	return (
-		<form className="search-box-form" onSubmit={props.handleSubmit}>
-			<Field 
-				component="input" 
-				name="query" 
-				type="text" 
-				autoComplete="off" 
-				autoCorrect="off" 
-				autoCapitalize="off" 
-				spellCheck="false" 
-				autofocus="on" 
-				id="searchField" />
-    </form>
-	);
+	componentDidMount() {
+		this.refs.queryRef.getRenderedComponent().focus()
+	}
+
+	render() {
+
+		return (
+			<form className="search-box-form" onSubmit={this.props.handleSubmit}>
+				<Field
+					withRef
+					ref="queryRef"
+					component="input" 
+					name="query" 
+					type="text" 
+					autoComplete="off" 
+					autoCorrect="off" 
+					autoCapitalize="off" 
+					spellCheck="false" 
+					autofocus="on" 
+					id="searchField" />
+	    </form>
+		);
+	}
 }
 
 SearchBox = reduxForm({
