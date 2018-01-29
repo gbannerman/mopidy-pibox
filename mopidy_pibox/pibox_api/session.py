603d0675c2e0fe068112053f85678dd6e8ad47b4
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-01-29 17:30:37 +0000

    fixed constructor call for session

--- mopidy_pibox/pibox_api/session.py
+++ mopidy_pibox/pibox_api/session.py
@@ -1,6 +1,6 @@
 class PiboxSession(object):
 
-    def __init__(self, skipThreshold):
+    def __init__(self, skip_threshold):
       self.skip_threshold = skip_threshold
       self.blacklist = ['spotify:track:0afhq8XCExXpqazXczTSve']
       self.votes = {}
