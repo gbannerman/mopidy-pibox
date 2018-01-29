5d83a79abefd2a1ff3f6f5de65272caed4cf7839
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-01-29 17:28:16 +0000

    fixed indentation errors

--- mopidy_pibox/pibox_api/session.py
+++ mopidy_pibox/pibox_api/session.py
@@ -1,7 +1,7 @@
 class PiboxSession(object):
 
     def __init__(self, skipThreshold):
-  		self.skip_threshold = skip_threshold
-    	self.blacklist = ['spotify:track:0afhq8XCExXpqazXczTSve']
+      self.skip_threshold = skip_threshold
+      self.blacklist = ['spotify:track:0afhq8XCExXpqazXczTSve']
       self.votes = {}
       self.has_voted = {}
