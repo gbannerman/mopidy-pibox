d0422e34de611e8416f120117b185a5daa20aec9
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-01-30 17:15:46 +0000

    moved toasts out to service

--- src/services/pibox.js
+++ src/services/pibox.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export function submitSkipVote(fingerprint, track) {
 	return (axios.post('/pibox/api/vote', {
     uri: track.uri,
-    fingerprint:fingerprint
+    fingerprint: fingerprint
 	}));
 }
