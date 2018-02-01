d778189abffef1ac857d07052acbb48400187ffb
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-02-01 14:29:22 +0000

    modified search icon to use material library

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,22 @@ import NowPlaying from './NowPlaying.jsx';
 import Tracklist from './Tracklist.jsx';
 import '../style/Home.css';
 import { Link } from 'react-router-dom';
+import Search from 'material-ui-icons/Search';
 
 export default class Home extends React.Component {
 
 	render() {
 
+		const iconStyle = {
+      width: 45,
+      height: 45
+    }
+
 		return (
 			<div className="home">
         <ul>
         	<li className="nav-item-title"><h2 className="nav-title">pibox</h2></li>
-        	<li className="nav-item-search"><Link className="Link" to="/pibox/search/"><img className="icon" alt="search icon" src="https://d30y9cdsu7xlg0.cloudfront.net/png/14173-200.png" /></Link></li>
+        	<li className="nav-item-search"><Link className="Link" to="/pibox/search/"><Search style={iconStyle}/></Link></li>
         </ul>
 	      <NowPlaying playback={this.props.playback} />
 	      <Tracklist mopidy={this.props.mopidy} tracks={this.props.tracklist} display={3} voteToSkip={this.props.voteToSkip}/>
