782d3e84733a2a8995ebee913911d0c3fa1ff5fa
Gavin Bannerman <gbannerman@scottlogic.co.uk>
2018-02-12 14:23:34 +0000

    bumped version numbers

--- mopidy_pibox/__init__.py
+++ mopidy_pibox/__init__.py
@@ -7,7 +7,7 @@ import tornado.web
 from mopidy import config, ext
 from pibox_api import api, session, socket
 
-__version__ = '0.5.5'
+__version__ = '0.6.0'
 
 # TODO: If you need to log, use loggers named after the current Python module
 logger = logging.getLogger(__name__)
