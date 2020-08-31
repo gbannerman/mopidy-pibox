32ebcdd5015c751cc56bb142305bc9bd3e19ff8c
Gavin Bannerman <bannermangavin@gmail.com>
2020-08-31 21:45:17 +0100

    Initial admin flag added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NowPlaying from "./NowPlaying.jsx";
 import Tracklist from "./Tracklist.jsx";
 import { Link, Route } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import { IconButton } from "@material-ui/core";
 import SearchOverlay from "./SearchOverlay.jsx";
+import { useAdmin } from "hooks/admin";
+import { useSnackbar } from "notistack";
 import "../style/Home.css";
 
 const Home = () => {
+  const { isAdmin, setIsAdmin } = useAdmin();
+
+  const [adminCounter, setAdminCounter] = useState(0);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    if (isAdmin || adminCounter < 7) {
+      return;
+    }
+
+    setIsAdmin(true);
+    enqueueSnackbar(`You are now an admin!`, { variant: "success" });
+  }, [adminCounter, isAdmin, setIsAdmin, enqueueSnackbar]);
+
   return (
     <div>
       <div className="home">
         <ul className="nav-bar">
           <li className="nav-item-title">
-            <h2 className="nav-title">pibox</h2>
+            <h2
+              className="nav-title"
+              onClick={() => setAdminCounter(adminCounter + 1)}
+            >
+              pibox
+            </h2>
           </li>
           <li className="nav-item-search">
             <Link className="Link" to="/search/">
