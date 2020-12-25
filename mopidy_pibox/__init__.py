b28c51cb8d1708c03f0944b299fb60e2eaab50b0
Gavin Bannerman <gavinb@administrate.co>
2020-12-25 23:06:35 +0000

    Bump version to 0.8.1

--- mopidy_pibox/__init__.py
+++ mopidy_pibox/__init__.py
@@ -7,7 +7,7 @@ from mopidy import config, ext
 from .pibox_api import api, session, socket
 from .routing import ClientRoutingHandler
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 def my_app_factory(config, core):
 
