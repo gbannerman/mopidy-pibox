0f3fdb798a729d51fbd6b3bb7a3e46e7cdde14da
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2022-01-04 00:38:11 +0000

    v0.8.4

--- mopidy_pibox/__init__.py
+++ mopidy_pibox/__init__.py
@@ -7,7 +7,7 @@ from mopidy import config, ext
 from .pibox_api import api, session, socket
 from .routing import ClientRoutingHandler
 
-__version__ = '0.8.3'
+__version__ = '0.8.4'
 
 def my_app_factory(config, core):
 
