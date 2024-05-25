88fb9cdafa0d608c841578e3f0e1d264233ed263
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2024-05-25 15:56:57 +0100

    Patch release

--- mopidy_pibox/__init__.py
+++ mopidy_pibox/__init__.py
@@ -9,7 +9,7 @@ from . import api
 from . import socket
 from .routing import ClientRoutingHandler
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 def get_http_handlers(core, config, frontend, static_directory_path):
