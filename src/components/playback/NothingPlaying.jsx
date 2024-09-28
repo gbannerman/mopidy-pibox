da5e62f41962842766839b4f03e27a35cfc7ad5c
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2024-09-28 15:35:05 +0100

    Fix numbers in steps on nothing playing page

--- src/components/playback/NothingPlaying.jsx
+++ src/components/playback/NothingPlaying.jsx
@@ -7,7 +7,7 @@ const NothingPlaying = () => {
     <div className="flex flex-wrap justify-center flex-col items-center">
       <h2>Welcome to pibox!</h2>
       <img className="w-[70px] h-auto m-1" alt="logo" src={logo} />
-      <ol type="1">
+      <ol className="list-decimal" type="1">
         <li className="p-1">Tap the search icon at the top right</li>
         <li className="p-1">Search for an artist, song or album</li>
         <li className="p-1">Tap on the song you want to queue</li>
