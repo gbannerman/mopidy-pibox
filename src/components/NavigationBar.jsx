807e691b1761def7db8f7d58fe2cc6492b02730d
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2024-10-02 19:29:43 +0100

    Extract Admin counter logic to hook

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "wouter";
 import SearchIcon from "@mui/icons-material/Search";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { IconButton } from "@mui/material";
 import { useAdmin } from "hooks/admin";
-import { useSnackbar } from "notistack";
 
 const NavigationBar = () => {
-  const { isAdmin, setIsAdmin } = useAdmin();
-
-  const [adminCounter, setAdminCounter] = useState(0);
-
-  const { enqueueSnackbar } = useSnackbar();
-
-  useEffect(() => {
-    if (isAdmin || adminCounter < 7) {
-      return;
-    }
-
-    setIsAdmin(true);
-    enqueueSnackbar(`You are now an admin!`, { variant: "success" });
-  }, [adminCounter, isAdmin, setIsAdmin, enqueueSnackbar]);
+  const { isAdmin, triggerSecretAdminAction } = useAdmin();
 
   return (
     <ul className="flex justify-between items-center list-none m-0 px-2">
@@ -28,7 +14,7 @@ const NavigationBar = () => {
         {!isAdmin ? (
           <h2
             className="text-start inline-block font-bold text-xl pl-2"
-            onClick={() => setAdminCounter(adminCounter + 1)}
+            onClick={triggerSecretAdminAction}
           >
             pibox
           </h2>
--- src/hooks/admin.js
+++ src/hooks/admin.js
@@ -1,8 +1,9 @@
-import React, { useState, useCallback } from "react";
+import { useSnackbar } from "notistack";
+import React, { useState, useCallback, useEffect } from "react";
 
 const defaultAdminContext = {
   isAdmin: false,
-  setIsAdmin: () => {},
+  triggerSecretAdminAction: () => {},
 };
 
 export const AdminContext = React.createContext(defaultAdminContext);
@@ -11,11 +12,22 @@ export const useAdmin = () => React.useContext(AdminContext);
 
 export const useAdminContext = () => {
   const [admin, setAdmin] = useState(false);
+  const [adminSecretActionCounter, setAdminSecretActionCounter] = useState(0);
+  const { enqueueSnackbar } = useSnackbar();
 
-  const setIsAdmin = useCallback(setAdmin, []);
+  useEffect(() => {
+    if (!admin && adminSecretActionCounter >= 7) {
+      setAdmin(true);
+      enqueueSnackbar("You are now an admin", { variant: "success" });
+    }
+  }, [admin, adminSecretActionCounter, enqueueSnackbar]);
+
+  const triggerSecretAdminAction = useCallback(() => {
+    setAdminSecretActionCounter((prev) => prev + 1);
+  }, [admin]);
 
   return {
     isAdmin: admin,
-    setIsAdmin,
+    triggerSecretAdminAction,
   };
 };
