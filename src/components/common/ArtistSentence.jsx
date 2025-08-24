9391427190d676db9d40883eedd5a62b0ec2e927
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2025-08-24 17:33:28 +0100

    Refactor ArtistSentence

--- src/components/common/ArtistSentence.jsx
+++ src/components/common/ArtistSentence.jsx
@@ -1,45 +1,37 @@
 import React from "react";
 
-export default class ArtistSentence extends React.Component {
-  render() {
-    if (!this.props.artists) {
-      return <span>-</span>;
-    }
-
-    return (
-      <span
-        className={
-          this.props.className
-            ? this.props.className + " artist-sentence"
-            : "artist-sentence"
-        }
-      >
-        {this.props.artists.map((artist, index) => {
-          if (!artist) {
-            return <span key={index}>-</span>;
-          }
-
-          var separator = null;
-          if (index === this.props.artists.length - 2) {
-            separator = " and ";
-          } else if (index < this.props.artists.length - 2) {
-            separator = ", ";
-          }
-
-          let content = <span>-</span>;
-
-          if (artist) {
-            content = <span>{artist.name}</span>;
-          }
-
-          return (
-            <span key={"artist_" + index}>
-              {content}
-              {separator}
-            </span>
-          );
-        })}
-      </span>
-    );
+export default function ArtistSentence({ artists }) {
+  if (!artists || !artists.length) {
+    return <span>Unknown Artist</span>;
   }
+
+  return (
+    <span className="artist-sentence">
+      {artists.map((artist, index) => {
+        if (!artist) {
+          return <span key={index}>Unknown Artist</span>;
+        }
+
+        var separator = null;
+        if (index === artists.length - 2) {
+          separator = " and ";
+        } else if (index < artists.length - 2) {
+          separator = ", ";
+        }
+
+        let content = <span>-</span>;
+
+        if (artist) {
+          content = <span>{artist.name}</span>;
+        }
+
+        return (
+          <span key={"artist_" + index}>
+            {content}
+            {separator}
+          </span>
+        );
+      })}
+    </span>
+  );
 }
