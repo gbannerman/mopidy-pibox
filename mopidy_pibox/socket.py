e61f8587cccff1cdb0f1472e7460a78c7fb38325
Gavin Bannerman <16233190+gbannerman@users.noreply.github.com>
2026-07-19 17:21:14 +0100

    Fix types in mopidy_pibox/socket.py

--- mopidy_pibox/socket.py
+++ mopidy_pibox/socket.py
@@ -8,21 +8,21 @@ class PiboxWebSocket(tornado.websocket.WebSocketHandler):
     clients: ClassVar[set] = set()
     logger = logging.getLogger(__name__)
 
-    def check_origin(self, origin):  # noqa: ARG002
+    def check_origin(self, origin: str) -> bool:  # noqa: ARG002
         return True
 
-    def open(self):
+    def open(self) -> None:
         self.clients.add(self)
         self.logger.debug("WebSocket opened")
 
-    def on_message(self, message):
+    def on_message(self, message: str | bytes) -> None:
         self.logger.info(message)
 
-    def on_close(self):
+    def on_close(self) -> None:
         self.clients.remove(self)
         self.logger.debug("WebSocket closed")
 
     @classmethod
-    def send(cls, subject, message):
+    def send(cls, subject: str, message: object) -> None:
         for conn in cls.clients:
             conn.write_message({"type": subject, "payload": message})
